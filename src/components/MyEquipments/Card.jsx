91de1d82156108d1012ba9d59da0da566cbcfbde
Durjoy96 <mddurjoy853@gmail.com>
2024-12-07 13:55:20 +0600

    delete functionality complete

--- src/components/MyEquipments/Card.jsx
+++ src/components/MyEquipments/Card.jsx
@@ -1,19 +1,40 @@
 import { FaEdit, FaTrash } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
-const Card = ({ equipment }) => {
-  const {
-    category,
-    customization,
-    description,
-    imageURL,
-    itemName,
-    price,
-    processingTime,
-    rating,
-    stockStatus,
-    _id,
-  } = equipment;
+const Card = ({ equipment, filteredDataHandler }) => {
+  const { imageURL, itemName, _id } = equipment;
+  const deleteHandler = (id) => {
+    console.log(id);
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#d33",
+      cancelButtonColor: "#FF8000",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/equipment/${id}`, {
+          method: "DELETE",
+          headers: {
+            "content-type": "application/json",
+          },
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            data.acknowledged && filteredDataHandler(id);
+            data.acknowledged &&
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+          });
+      }
+    });
+  };
   return (
     <>
       <div className="bg-base-100 p-8 rounded-lg shadow-sm flex flex-col">
@@ -29,7 +50,10 @@ const Card = ({ equipment }) => {
             {itemName}
           </h2>
           <div className="mt-6 flex gap-6">
-            <button className="flex-1 flex items-center gap-2 bg-transparent px-4 py-2 rounded-lg text-red-500 border border-red-500 hover:opacity-80">
+            <button
+              onClick={() => deleteHandler(_id)}
+              className="flex-1 flex items-center gap-2 bg-transparent px-4 py-2 rounded-lg text-red-500 border border-red-500 hover:opacity-80"
+            >
               <FaTrash></FaTrash>
               Delete
             </button>
--- src/pages/MyEquipments.jsx
+++ src/pages/MyEquipments.jsx
@@ -1,15 +1,26 @@
 import { useLoaderData } from "react-router-dom";
 import Card from "../components/MyEquipments/Card";
+import { useState } from "react";
 
 const MyEquipments = () => {
   const data = useLoaderData();
   console.log(data);
+  const [equipmentData, setEquipmentData] = useState(data);
+
+  const filteredDataHandler = (id) => {
+    const remainingData = data.filter((equipment) => equipment._id !== id);
+    setEquipmentData(remainingData);
+  };
   return (
     <>
       <div className="max-w-screen-xl mx-auto mt-12">
-        <div className="grid gap-6 grid-cols-4">
-          {data.map((equipment, idx) => (
-            <Card key={idx} equipment={equipment}></Card>
+        <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+          {equipmentData.map((equipment, idx) => (
+            <Card
+              key={idx}
+              equipment={equipment}
+              filteredDataHandler={filteredDataHandler}
+            ></Card>
           ))}
         </div>
       </div>
